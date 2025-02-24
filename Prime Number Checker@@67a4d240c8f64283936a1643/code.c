c6822e5fcecd12efaee912e2d5fed243f1755709
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-02-24 10:56:32 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,40 +1,37 @@
-# include<stdio.h>
-# include<math.h>
-void checkPrime(int arr[],int n);
-int main(){
+#include <stdio.h>
+#include <math.h>
+
+void checkPrime(int arr[], int n);
+
+int main() {
     int t;
-    scanf("%d",&t);
-    
+    scanf("%d", &t);
+
     int arr[t];
-    for(int i=0;i<t;i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < t; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    checkPrime(arr,t);
-   
+    checkPrime(arr, t);
 }
 
-void checkPrime(int arr[],int n){
-    for(int i=0;i<n;i++){
+void checkPrime(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
         int isPrime = 1;
-        if(arr[i] == 1){
-            printf("0");
-        }
-        else{
-            for(int j=2;j<n;j++){
-                if(arr[i]%j == 0){
+        if (arr[i] == 1) {
+            printf("0 ");
+        } else {
+            for (int j = 2; j <= sqrt(arr[i]); j++) {
+                if (arr[i] % j == 0) {
                     isPrime = 0;
                     break;
                 }
             }
-            if(isPrime){
-                printf("1");
+            if (isPrime) {
+                printf("1 ");
+            } else {
+                printf("0 ");
             }
-            else{
-                printf("0");
-            }
-
         }
     }
-    
 }
