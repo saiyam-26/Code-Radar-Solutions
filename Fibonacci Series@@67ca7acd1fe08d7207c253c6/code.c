9b183ed1b5c96c88a28159b5428dd8ae3965efef
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-03-07 15:00:28 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Recursive function to compute the nth Fibonacci number
+
 int fibonacci(int n) {
     if (n == 0) {
         return 0;
@@ -11,10 +11,10 @@ int fibonacci(int n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-// Function to print the Fibonacci series using recursion
+
 void fibonacciSeries(int n) {
     for (int i = 0; i < n; i++) {
-        printf("%d ", fibonacci(i));  // Calling the recursive function
+        printf("%d ", fibonacci(i));  
     }
     printf("\n");
 }
