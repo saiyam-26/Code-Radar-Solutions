a2282aa01a21803b9cd6bd0fdf1d93bb664c1552
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-03-07 14:57:43 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,9 +1,12 @@
 int fibonacciSeries(int n) {
     if (n == 0) {
-        printf("0");
+        return 0;
     } 
     else if (n == 1) {
-        printf("1");
+        return 1;
     } 
     return fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
+}
+for(int i=0;i<n;i++){
+    fibonacciSeries(i);
 }
